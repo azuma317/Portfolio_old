df43d4fd81298334d59e511ade4012b390f4a0f3
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-08-13 23:16:49 +0900

    Add gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-175317044-1`,
       },
     },
     `gatsby-plugin-feed`,
