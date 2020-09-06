2ba81c3962ffe040db4002fe6b7358cdadbd0677
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-09-06 23:01:22 +0900

    Add header.tsx

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import config from '../config/blog-config'
+import { Props } from '@fortawesome/react-fontawesome'
+
+type Props = {
+  activePage: string
+}
+
+const Header: React.FC<Props> = activePage => {}
+
+export default Header
