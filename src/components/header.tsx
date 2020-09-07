3b114097b9285e5e776523ab9456945618db044f
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-09-07 23:19:01 +0900

    Add css in Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,6 +6,13 @@ type Props = {
   activePage: string
 }
 
-const Header: React.FC<Props> = activePage => {}
+const Header: React.FC<Props> = activePage => {
+  return <header css={style.header_container}></header>
+}
 
 export default Header
+
+const style = {
+  header_container: css`
+  `,
+}
