f10d05448ad299825d0e44afd86928d4a6656fee
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-09-07 23:48:12 +0900

    Update header_container of css in Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,5 +14,22 @@ export default Header
 
 const style = {
   header_container: css`
+    center: true;
+    margin: auto;
+    max-width: 1280px;
+    height: 8rem;
+    overflow: hidden;
+    @media (max-width: 640px) {
+      padding-left: 1.25rem;
+      padding-right: 1.25rem;
+    }
+    @media (min-width: 640px) and (max-width: 768px) {
+      padding-left: 3rem;
+      padding-right: 3rem;
+    }
+    @media (min-width: 768px) {
+      padding-left: 5rem;
+      padding-right: 5rem;
+    }
   `,
 }
