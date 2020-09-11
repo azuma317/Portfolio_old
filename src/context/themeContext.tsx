4f7fa4eaafa7d7194964b04a0f7efebd7dfa57be
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-09-11 22:56:19 +0900

    Update themeContext

---
+++ src/context/themeContext.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { ThemeContext } from '@emotion/core'
+
+type Props = {
+  initialTheme: string
+}
+
+const getInitialTheme: () => string = () => {
+  if (window.localStorage) {
+    const storedPrefs = window.localStorage.getItem('color-theme')
+    if (typeof storedPrefs === 'string') {
+      return storedPrefs
+    }
+
+    const userMedia = window.matchMedia('(prefers-color-scheme: dark)')
+    if (userMedia.matches) {
+      return 'dark'
+    }
+  }
+  return 'dark'
+}
+
+// export const ThemeContext = React.createContext()
+
+const ThemeProvider: React.FC<Props> = ({ initialTheme, children }) => {
+  const [theme, setTheme] = React.useState(getInitialTheme)
+
+  const rawSetTheme = (theme: string) => {
+    const root = window.document.documentElement
+    const isDark = theme === 'dark'
+
+    root.classList.remove(isDark ? 'light' : 'dark')
+    root.classList.add(theme)
+
+    localStorage.setItem('color-theme', theme)
+  }
+
+  if (initialTheme) {
+    rawSetTheme(initialTheme)
+  }
+
+  React.useEffect(() => {
+    rawSetTheme(theme)
+  }, [theme])
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeProvider
