fb6ca6dceaf775d9bca489433be9c4462b04f37a
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-09-12 15:22:21 +0900

    Add css of main DOM in layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import type { WindowLocation } from '@reach/router'
+import { css } from '@emotion/core'
 import ThemeProvider from '../context/themeContext'
 import Header from './header'
 import Footer from './footer'
@@ -13,7 +14,7 @@ const Layout: React.FC<Props> = ({ location, children }) => {
     <>
       <ThemeProvider>
         <Header activePage={location.pathname} />
-        <main>{children}</main>
+        <main css={styles.main}>{children}</main>
         <Footer />
       </ThemeProvider>
     </>
@@ -22,4 +23,28 @@ const Layout: React.FC<Props> = ({ location, children }) => {
 
 export default Layout
 
-const styles = {}
+const styles = {
+  main: css`
+    display: block;
+    margin: auto;
+    max-width: 1280px;
+    width: 100%;
+    overflow: hidden;
+    margin-top: 4rem;
+    padding-left: 1.25rem;
+    padding-right: 1.25rem;
+    @media (min-width: 640px) and (max-width: 768px) {
+      max-width: 640px;
+      padding-left: 3rem;
+      padding-right: 3rem;
+    }
+    @media (min-width: 768px) {
+      max-width: 768px;
+      padding-left: 5rem;
+      padding-right: 5rem;
+    }
+    @media (min-width: 1024px) {
+      max-width: 1024px;
+    }
+  `,
+}
