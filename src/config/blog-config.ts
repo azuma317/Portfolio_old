21a578f5e0f104f9b7cc35cfa167785ad23e7c0a
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-09-13 13:00:16 +0900

    Delete blog-config file

--- src/config/blog-config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  title: 'Azuma Blog',
-  description: "Infra Engineer's tech blog.",
-  blogUrl: 'https://pages.azumax.work/',
-  blogAuthor: 'Azuma Sato',
-}
-
-export default config
