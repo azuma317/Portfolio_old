272106e45bcc025a00ea6f99fbe7999a1a009e2d
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-09-15 23:29:43 +0900

    Add ThemeToggle

---
+++ src/components/themeToggle.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { css } from '@emotion/core'
+import { ThemeContext } from '../context/themeContext'
+
+type Props = {
+  className: string
+}
+
+const ThemeToggle: React.FC<Props> = ({ className }) => {
+  const { theme, setTheme } = React.useContext(ThemeContext)
+
+  function isLight(): boolean {
+    return theme === 'light'
+  }
+
+  const ToggleTheme = () => {
+    setTheme(isLight() ? 'dark' : 'light')
+  }
+
+  return (
+    <>
+      <button
+        aria-label={isLight() ? 'Activate Dark Mode' : 'Activate Light Mode'}
+        title={isLight() ? 'Activate Dark Mode' : 'Activate Light Mode'}
+        onClick={ToggleTheme}
+      ></button>
+    </>
+  )
+}
