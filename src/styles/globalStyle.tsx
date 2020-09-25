3209c0171944563b7f9b5c980250d1c8dd72dc94
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-09-25 23:25:31 +0900

    Update transition in GlobalStyle

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -66,7 +66,7 @@ const styles = css`
     --color-code-regex: #ffb86c;
   }
   body {
-    transition: all 300ms ease 0s;
+    // transition: all 300ms ease 0s;
     overflow-x: hidden;
     background-color: var(--color-bg-primary);
     color: var(--color-text-primary);
