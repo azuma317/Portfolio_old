50ceb3a6eec4175414eae7a1b746cafd5c5a9887
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-10-16 00:16:47 +0900

    Add style in 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,17 @@
-import React from "react"
+import React from 'react'
+import { css } from '@emotion/core'
 
 const NotFoundPage: React.FC<unknown> = () => (
-  <div>
+  <div css={styles.not_found}>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
 )
 
 export default NotFoundPage
+
+const styles = {
+  not_found: css`
+    text-align: center;
+  `,
+}
