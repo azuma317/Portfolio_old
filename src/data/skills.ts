85d208a482367cb63f77fa7112b62342f74e2774
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-10-17 13:14:39 +0900

    Add Skills data

---
+++ src/data/skills.ts
@@ -0,0 +1,40 @@
+export default [
+  {
+    label: 'Infrastructure Engineer Skills',
+    description: 'IT企業に1年半ほど在籍していたときに習得していたスキル',
+    categories: [
+      {
+        title: 'Infrastructure engineer',
+        skills: [
+          'Apache, Tomcat の設定ファイル管理',
+          '開発環境のDocker化',
+          'AWS(EC2, CloudWatch, Lambda, S3, Kinesis)によるインフラ構築及び管理',
+        ],
+      },
+    ],
+  },
+  {
+    label: 'iOS Development Skills',
+    description: '学生の頃から趣味で始めたiOSアプリ開発で習得したスキル',
+    categories: [
+      {
+        title: 'iOS Development',
+        skills: ['Storybaordを使用したUI作成'],
+      },
+      {
+        title: 'Backend Development',
+        skills: ['Firebase(Firestore, Storage, Functions)によるインフラ構築'],
+      },
+    ],
+  },
+  {
+    label: 'Web Development Skills',
+    description: 'Portfolio作成で習得したスキル',
+    categories: [
+      {
+        title: 'Front-end Development',
+        skills: ['React, TypeScript, Gatsby'],
+      },
+    ],
+  },
+]
