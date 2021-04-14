d5d957f1bbb94cb1f45b12e701ec1516a229821c
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-04-14 23:21:10 +0900

    Add tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: ['./components/**/*.tsx', './pages/**/*.tsx', './public/**/*.html'],
+  darkMode: false,
+  theme: {
+    fontFamily: {
+      serif: ['Merriweather', 'Georgia', 'serif'],
+    },
+    extend: {},
+  },
+}
