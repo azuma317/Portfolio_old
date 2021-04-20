50ca18f12fa5a0fc239358e1e988f2428921b94b
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-04-20 23:58:24 +0900

    Update tailwindcss in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -39,15 +39,18 @@ const Footer: React.FC<Props> = ({ activePage }) => {
           <div tw="mb-2 mt-4">
             <Nav activePage={activePage} justify="end" />
           </div>
-          <div css={styles.footer_info}>
-            <span css={styles.footer_copyright} aria-label="Copyright">
+          <div tw="flex flex-col-reverse md:flex-row md:justify-between md:mt-8">
+            <span
+              tw="self-center text-xs tracking-wider md:self-end"
+              aria-label="Copyright"
+            >
               © 2020 {author.blogAuthor}. All Right Reserved.
             </span>
-            <div css={styles.footer_sns}>
-              <span>
+            <div tw="flex-flow flex items-center justify-center md:justify-end">
+              <span tw="ml-4">
                 <a
                   href={'https://github.com/' + social.github}
-                  css={styles.footer_icon}
+                  tw="text-secondary block align-middle w-8 h-8"
                   aria-label="Visit Github profile"
                   title="Visit Github profile"
                   target="_blank"
@@ -56,10 +59,10 @@ const Footer: React.FC<Props> = ({ activePage }) => {
                   <AiFillGithub />
                 </a>
               </span>
-              <span>
+              <span tw="ml-4">
                 <a
                   href={'https://twitter.com/' + social.twitter}
-                  css={styles.footer_icon}
+                  tw="text-secondary block align-middle w-8 h-8"
                   aria-label="Visit Twitter profile"
                   title="Visit Twitter profile"
                   target="_blank"
@@ -68,10 +71,10 @@ const Footer: React.FC<Props> = ({ activePage }) => {
                   <AiFillTwitterCircle />
                 </a>
               </span>
-              <span>
+              <span tw="ml-4">
                 <a
                   href={'https://www.instagram.com/' + social.github}
-                  css={styles.footer_icon}
+                  tw="text-secondary block align-middle w-8 h-8"
                   aria-label="Visit Instagram profile"
                   title="Visit Instagram profile"
                   target="_blank"
@@ -89,94 +92,3 @@ const Footer: React.FC<Props> = ({ activePage }) => {
 }
 
 export default Footer
-
-const styles = {
-  footer_container: css`
-    display: block;
-    padding-top: 3rem;
-    padding-bottom: 3rem;
-    margin: auto;
-    height: 100%;
-    width: 100%;
-    @media (max-width: 640px) {
-      padding-left: 1.25rem;
-      padding-right: 1.25rem;
-    }
-    @media (min-width: 640px) and (max-width: 768px) {
-      max-width: 640px;
-      padding-left: 3rem;
-      padding-right: 3rem;
-    }
-    @media (min-width: 768px) {
-      max-width: 768px;
-      padding-left: 5rem;
-      padding-right: 5rem;
-    }
-    @media (min-width: 1024px) {
-      max-width: 1024px;
-    }
-  `,
-  footer_nav: css`
-    display: none;
-    font-size: 0.875rem;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
-    @media (max-width: 640px) {
-      padding-left: 1.25rem;
-      padding-right: 1.25rem;
-    }
-    @media (min-width: 640px) and (max-width: 768px) {
-      max-width: 640px;
-      padding-left: 3rem;
-      padding-right: 3rem;
-    }
-    @media (min-width: 768px) {
-      justify-content: flex-end;
-    }
-    @media (min-width: 1024px) {
-      display: flex;
-    }
-    a:nth-of-type(n + 2) {
-      margin-left: 1rem;
-    }
-  `,
-  footer_info: css`
-    display: flex;
-    flex-direction: column-reverse;
-    @media (min-width: 768px) {
-      justify-content: space-between;
-      flex-direction: row;
-      margin-top: 2rem;
-    }
-  `,
-  footer_copyright: css`
-    letter-spacing: 0.05em;
-    font-size: 0.75rem;
-    align-self: center;
-    @media (min-width: 768px) {
-      align-self: flex-end;
-    }
-  `,
-  footer_sns: css`
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    justify-content: center;
-    @media (min-width: 768px) {
-      justify-content: flex-end;
-    }
-    span {
-      margin-left: 1rem;
-    }
-  `,
-  footer_icon: css`
-    svg {
-      display: block;
-      vertical-align: middle;
-      width: 2rem;
-      height: 2rem;
-      color: var(--color-text-secondary);
-    }
-  `,
-}
