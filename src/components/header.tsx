55c390bea9c3a4702b94e55a2e7064adac2f71c9
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-05-06 00:37:07 +0900

    Update mobile design in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,86 @@
 import React from 'react'
-import 'twin.macro'
-import Nav from './atoms/nav'
+import { Disclosure } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import tw, { theme } from 'twin.macro'
+import { css } from '@emotion/core'
 
 type Props = {
   activePage: string
 }
 
+const navigation = [
+  { name: 'Home', href: '/' },
+  { name: 'Blog', href: '/blog/' },
+  { name: 'Projects', href: '/projects/' },
+  { name: 'Portfolio', href: '/portfolio/' },
+]
+
 const Header: React.FC<Props> = ({ activePage }) => (
-  <header tw="m-auto px-5 max-w-6xl h-0 overflow-hidden sm:px-12 sm:h-32 md:px-20">
-    <Nav activePage={activePage} justify="start" />
-  </header>
+  <Disclosure as="header">
+    {({ open }) => (
+      <>
+        <div tw="mx-auto px-5 max-w-6xl sm:px-12 sm:h-16 md:px-20 lg:px-8">
+          <div tw="relative flex items-center justify-between h-16">
+            <div tw="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <Disclosure.Button tw="inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
+                {open ? (
+                  <XIcon tw="block w-6 h-6" aria-hidden="true" />
+                ) : (
+                  <MenuIcon tw="block w-6 h-6" aria-hidden="true" />
+                )}
+              </Disclosure.Button>
+            </div>
+            <div tw="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div tw="hidden sm:block sm:ml-6">
+                <div tw="flex space-x-4">
+                  {navigation.map(item => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      css={[
+                        css`
+                          color: ${item.href === activePage
+                            ? theme('colors.accent')
+                            : theme('colors.secondary')};
+                        `,
+                        tw`px-3 py-2 text-base font-medium rounded-md`,
+                      ]}
+                      aria-current={
+                        item.href === activePage ? 'page' : undefined
+                      }
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Disclosure.Panel tw="sm:hidden">
+          <div tw="pb-3 pt-2 px-2 space-y-1">
+            {navigation.map(item => (
+              <a
+                key={item.name}
+                href={item.href}
+                css={[
+                  css`
+                    color: ${item.href === activePage
+                      ? theme('colors.accent')
+                      : theme('colors.secondary')};
+                  `,
+                  tw`block px-3 py-2 text-base font-medium rounded-md`,
+                ]}
+                aria-current={item.href === activePage ? 'page' : undefined}
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </Disclosure.Panel>
+      </>
+    )}
+  </Disclosure>
 )
 
 export default Header
