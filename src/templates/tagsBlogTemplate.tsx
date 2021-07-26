20577452b31ee38fc17fd029651303b4de2d21d3
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-07-27 00:24:54 +0900

    Add TagsBlogTemplate

---
+++ src/templates/tagsBlogTemplate.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import 'twin.macro'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Title from '../components/title'
+import Post from '../components/post'
+import { BlogByTagQuery } from '../../types/graphql-types'
+
+type Props = {
+  location: Location
+  data: BlogByTagQuery
+}
+
+const TagsBlogTemplate: React.FC<Props> = ({ location, data }) => {
+  const { edges, totalCount } = data.allMarkdownRemark
+  const title = data.site?.siteMetadata?.title || ''
+  const description = data.site?.siteMetadata?.description || ''
+
+  return (
+    <Layout location={location}>
+      <SEO title={title} description={description} />
+      <Title title={title} />
+      <h1>{title}</h1>
+      {edges.map(({ node }) => {
+        return (
+          <Post key={node.fields?.slug} post={node} />
+        )
+      })}
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query BlogByTag($tag: String!) {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
